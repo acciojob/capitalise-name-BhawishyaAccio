44433bb085f1e6006ebf01c05331a974be4e9dba
BhawishyaAccio <bhawishya@acciojob.com>
2025-05-13 00:12:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let ans=document.getElementById("fname");
+ans.addEventListener("blur",()=>
+	{
+        console.log("object")
+		ans.value=ans.value.toUpperCase()
+        console.log(ans.value)
+	})
+console.log(ans.value)
